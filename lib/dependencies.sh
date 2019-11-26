f5fd909487b5ececee5e6800c2d04b6ec2f42ebf
Sean Reilly <reillyse@gmail.com>
2019-11-26 14:43:07 -0800

    make it so we do not rebuild if we don't need to

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -54,23 +54,26 @@ run_prebuild_script() {
 }
 
 run_build_script() {
-  local build_dir=${1:-}
-  local has_build_script has_heroku_build_script
 
-  has_build_script=$(has_script "$build_dir/package.json" "build")
-  has_heroku_build_script=$(has_script "$build_dir/package.json" "heroku-postbuild")
+  echo "short-circuit this so we will not rebuild for tests"
 
-  if [[ "$has_heroku_build_script" == "true" ]] && [[ "$has_build_script" == "true" ]]; then
-    echo "Detected both \"build\" and \"heroku-postbuild\" scripts"
-    mcount "scripts.heroku-postbuild-and-build"
-    run_if_present "$build_dir" 'heroku-postbuild'
-  elif [[ "$has_heroku_build_script" == "true" ]]; then
-    mcount "scripts.heroku-postbuild"
-    run_if_present "$build_dir" 'heroku-postbuild'
-  elif [[ "$has_build_script" == "true" ]]; then
-    mcount "scripts.build"
-    run_if_present "$build_dir" 'build'
-  fi
+  # local build_dir=${1:-}
+  # local has_build_script has_heroku_build_script
+
+  # has_build_script=$(has_script "$build_dir/package.json" "build")
+  # has_heroku_build_script=$(has_script "$build_dir/package.json" "heroku-postbuild")
+
+  # if [[ "$has_heroku_build_script" == "true" ]] && [[ "$has_build_script" == "true" ]]; then
+  #   echo "Detected both \"build\" and \"heroku-postbuild\" scripts"
+  #   mcount "scripts.heroku-postbuild-and-build"
+  #   run_if_present "$build_dir" 'heroku-postbuild'
+  # elif [[ "$has_heroku_build_script" == "true" ]]; then
+  #   mcount "scripts.heroku-postbuild"
+  #   run_if_present "$build_dir" 'heroku-postbuild'
+  # elif [[ "$has_build_script" == "true" ]]; then
+  #   mcount "scripts.build"
+  #   run_if_present "$build_dir" 'build'
+  # fi
 }
 
 log_build_scripts() {
@@ -92,7 +95,7 @@ yarn_node_modules() {
 }
 
 yarn_prune_devdependencies() {
-  local build_dir=${1:-} 
+  local build_dir=${1:-}
 
   if [ "$NODE_ENV" == "test" ]; then
     echo "Skipping because NODE_ENV is 'test'"
@@ -106,7 +109,7 @@ yarn_prune_devdependencies() {
     echo "Skipping because YARN_PRODUCTION is '$YARN_PRODUCTION'"
     meta_set "skipped-prune" "true"
     return 0
-  else 
+  else
     cd "$build_dir" || return
     monitor "yarn-prune" yarn install --frozen-lockfile --ignore-engines --ignore-scripts --prefer-offline 2>&1
     meta_set "skipped-prune" "false"
@@ -180,7 +183,7 @@ npm_rebuild() {
 
 npm_prune_devdependencies() {
   local npm_version
-  local build_dir=${1:-} 
+  local build_dir=${1:-}
 
   npm_version=$(npm --version)
 
